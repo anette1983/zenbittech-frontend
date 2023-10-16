fba12c99c358e58cabf2c1923e7fc1fa9bac7c44
Hanna <anna_kon@bigmir.net>
2023-10-16 20:58:39 +0300

    deploy fix try

--- src/main.jsx
+++ src/main.jsx
@@ -25,7 +25,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     />
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/zenbittech/">
+        <BrowserRouter basename="/">
           <App />
         </BrowserRouter>
       </PersistGate>
